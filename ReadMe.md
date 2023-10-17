02303f2d2782cb00fc9c8c86b38bc1cea67894b9
HEYAHONG <2229388563@qq.com>
2023-10-17 17:46:39 +0800

    初始版本

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# 说明
+
+在使用[MSYS2](https://www.msys2.org/)或者[Cygwin](https://cygwin.com/l)的过程中希望在cmd.exe中直接使用其中的命令（如which、find、file等命令）。
+
+为使用相关命令，可直接将MSYS2或者Cygwin添加至PATH变量，这样做的缺点是容易与其它软件冲突（如git for windows）,导致一些奇奇怪怪的错误（主要是由于某些dll版本不对）。
+
+本仓库就是主要解决软件冲突问题。
+
+# 使用
+
+- 使用默认路径安装好MSYS2或者Cygwin，并在MSYS2或者Cygwin安装好相应软件包。
+- 单独准备一个目录，并将此目录添加至PATH变量。将本仓库的程序重命名为需要重定向的目标（如find.exe）,放入准备好的目录。
+
+# 原理
+
+通过程序名搜索某些路径，并通过execve执行。
+
---
+++ main.cpp
@@ -0,0 +1,89 @@
+﻿
+#include <string>
+#include <iostream>
+#include <vector>
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "process.h"
+#include "windows.h"
+#include "unistd.h"
+
+std::string GetAppFileName()
+{
+    char path[MAX_PATH]= {0};
+    GetModuleFileNameA(NULL,path,MAX_PATH);
+    if(strlen(path)>0)
+    {
+        size_t len=strlen(path);
+        for(size_t i=0; i<len; i++)
+        {
+            if(path[len-i-1]=='\\')
+            {
+                return std::string(&path[len-i]);
+            }
+        }
+    }
+    return std::string();
+}
+
+std::vector<std::string> TargetSearchPath=
+{
+    "\\msys64\\usr\\bin",
+    "\\msys64\\mingw32\\bin",
+    "\\msys64\\mingw64\\bin",
+    "\\cygwin64\\bin"
+};
+
+std::string SearchTargetPath()
+{
+    std::string AppName=GetAppFileName();
+
+    for(std::vector<std::string>::iterator it=TargetSearchPath.begin(); it!=TargetSearchPath.end(); it++)
+    {
+        std::string path=(*it);
+        //添加末尾的反斜杠(\)
+        if(path.c_str()[path.length()-1]!='\\')
+        {
+            path+="\\";
+        }
+        if(path.c_str()[1]!=':')
+        {
+            //不是带盘符的路径,添加盘符搜索
+            for(char i='A'; i<='Z'; i++)
+            {
+                char driver[3]= {i,':',0};
+                std::string realpath=std::string(driver)+path+AppName;
+                if(access(realpath.c_str(),0)==0)
+                {
+                    return realpath;
+                }
+            }
+        }
+        else
+        {
+            std::string realpath=path+AppName;
+            if(access(realpath.c_str(),0)==0)
+            {
+                return realpath;
+            }
+        }
+    }
+
+    return std::string();
+}
+
+int main(int argc,char* const argv[], char* const envp[])
+{
+    std::string TargetPath=SearchTargetPath();
+    if(TargetPath.empty())
+    {
+        return -1;
+    }
+    else
+    {
+        execve(TargetPath.c_str(),argv,envp);
+    }
+
+    return 0;
+}
