c6562afabeaa3f01193f55037c9c5f1734abcaed
HEYAHONG <2229388563@qq.com>
2023-10-18 10:30:30 +0800

    更新argv[0],设置环境变量，使用spawnv替代execve

--- main.cpp
+++ main.cpp
@@ -73,16 +73,75 @@ std::string SearchTargetPath()
     return std::string();
 }
 
+std::string SearchTargetDirPath()
+{
+    std::string AppName=GetAppFileName();
+
+    for(std::vector<std::string>::iterator it=TargetSearchPath.begin(); it!=TargetSearchPath.end(); it++)
+    {
+        std::string path=(*it);
+        //添加末尾的反斜杠(\)
+        if(path.c_str()[path.length()-1]!='\\')
+        {
+            path+="\\";
+        }
+        if(path.c_str()[1]!=':')
+        {
+            //不是带盘符的路径,添加盘符搜索
+            for(char i='A'; i<='Z'; i++)
+            {
+                char driver[3]= {i,':',0};
+                std::string realpath=std::string(driver)+path+AppName;
+                if(access(realpath.c_str(),0)==0)
+                {
+                    return std::string(driver)+path;
+                }
+            }
+        }
+        else
+        {
+            std::string realpath=path+AppName;
+            if(access(realpath.c_str(),0)==0)
+            {
+                return path;
+            }
+        }
+    }
+
+    return std::string();
+}
+
 int main(int argc,char* const argv[], char* const envp[])
 {
     std::string TargetPath=SearchTargetPath();
-    if(TargetPath.empty())
+    std::string TargetDirPath=SearchTargetDirPath();
+    if(TargetPath.empty() || TargetDirPath.empty())
     {
         return -1;
     }
     else
     {
-        execve(TargetPath.c_str(),argv,envp);
+        //生成新的argv
+        char * new_argv[argc+1]={0};
+        {
+            //使得argv[0]为目标自身路径
+            new_argv[0]=(char *)TargetPath.c_str();
+            //复制原有的数据
+            for(int i=1;i<argc;i++)
+            {
+                new_argv[i]=argv[i];
+            }
+        }
+
+        //设置PATH
+        const char *path_c_str=getenv("PATH");
+        if(path_c_str!=NULL)
+        {
+            putenv((std::string("PATH=")+TargetDirPath+";"+std::string(path_c_str)).c_str());
+        }
+
+
+        spawnv(_P_WAIT,TargetPath.c_str(),new_argv);
     }
 
     return 0;
